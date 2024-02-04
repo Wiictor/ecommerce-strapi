72fe98b1512e0c6b190f5a660c786b0388cf650c
Wiictor <wiictor96@gmail.com>
2024-02-04 17:05:16 +0100

    First push

---
+++ config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "ecommerce-strapi-games-wiictor.s3.eu-west-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "ecommerce-strapi-games-wiictor.s3.eu-west-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
